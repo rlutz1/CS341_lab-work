de57c2b56c9bea32b4216b163f907c3dc1cb6737
Roxanne Lutz <rlutz1@unm,edu>
2025-08-21 12:17:45 -0600

    2.59: end is x, rest is y

--- lab-work-1.c
+++ lab-work-1.c
@@ -4,6 +4,24 @@
 int x = 0x874321;
 
 /*
+ * write c expression that will yield a word consisting 
+ * of the LSB of x and remaining bytes of y
+ * exercise 2.59
+ */
+int lsbXRestY() {
+	int x = 0x89ABCDEF;
+	int y = 0x76543210;
+	
+	int mask = 0xFF; // 00000...11111111
+	int lsbX = x & mask; // 0x000000EF
+	
+	mask = ~0xFF; // 11111...00000000
+	int msbsY = y & mask;
+	
+	return msbsY + lsbX;
+} 
+
+/*
  * last byte to all 1s, all other bytes unchanged
  * exercise 2.12
  */
@@ -53,6 +71,11 @@ int main() {
 	
 	returnVal = lsbOnesOtherwiseSame();
 	printf("lsbOnesOtherwiseSame: ");
+	printf("%#010X", returnVal);
+	printf("\n");
+	
+	returnVal = lsbXRestY();
+	printf("lsbXRestY: ");
 	printf("%#010X", returnVal);
 	printf("\n");
 	
