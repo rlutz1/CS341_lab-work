50be7fb07c3ebd4a897bab8158017ea406dfa909
Roxanne Lutz <rlutz1@unm,edu>
2025-08-21 12:06:43 -0600

    cleaning up and adding structure for printing.

--- lab-work-1.c
+++ lab-work-1.c
@@ -1,28 +1,61 @@
 #include <stdio.h>
 
+// same test case for all below right now.
+int x = 0x874321;
+
+/*
+ * last byte to all 1s, all other bytes unchanged
+ * exercise 2.12
+ */
+int lsbOnesOtherwiseSame() {
+	int mask = 0xFF;
+	
+	return x | mask;
+}
+
 /*
  * snag the last byte, complement everything else.  
+ * exercise 2.12
  */
 int lsbOtherwiseComplement() {
-	int x = 0x874321;
 	int mask = 0xFF;
 	
 	return x ^ ~mask;
 }
 
 /*
- * snag the last byte, but nothing else.  
+ * snag the last byte, but nothing else.
+ * exercise 2.12
  */
 int lsbOtherwiseZero() {
-	int x = 0x874321;
 	int mask = 0xFF;
 	
 	return x & mask;
 }
 
+/*
+ * run everything baby doll
+ */
 int main() {
+	printf("Test case: ");
+	printf("%#010X", + x);
+	printf("\n");
+	
 	int returnVal = lsbOtherwiseZero();
+	printf("lsbOtherwiseZero: ");
+	printf("%#010X", + returnVal);
+	printf("\n");
+	
+	returnVal = lsbOtherwiseComplement();
+	printf("lsbOtherwiseComplement: ");
 	printf("%#010X", returnVal);
+	printf("\n");
+	
+	returnVal = lsbOnesOtherwiseSame();
+	printf("lsbOnesOtherwiseSame: ");
+	printf("%#010X", returnVal);
+	printf("\n");
+	
 	return 0;
 }
 
