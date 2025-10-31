4c7b528f3c779ff65d0a100e6d2c6b6b0b882275
Roxanne Lutz <rlutz1@unm,edu>
2025-10-31 13:15:38 -0600

    starting up some arg parsing.

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -2,6 +2,7 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>  
 
 // struct definitions for ease of reading
 typedef struct  {
@@ -21,21 +22,30 @@ typedef struct {
     Set sets[];
 } Cache;
 
+typedef struct {
+    int numSets;
+    int numSetIndexBits;
+    int numLines;
+    int numBlocks;
+    int numBlockBits;
+    char *traceFilename;
+} SetupInfo;
+
 // globals
 int hits = 0;
 int misses = 0;
 int evictions = 0;
 
 // method declarations
-void extractArgs();
+SetupInfo extractArgs(int argc, char *argv[]);
 void parseFile();
 void tryReadCache(void *addr);
 void tryWriteCach(void *addr);
 
-int main()
+int main(int argc, char *argv[])
 {
     // (1) extract all arguments and values 
-    // -- extractArgs()
+    SetupInfo si = extractArgs(argc, argv);
     // (2) parse the file given into iterable lines. only needed data. 
     // -- parseFile()
     // (3) go through lines and attempt to interact through cache
@@ -45,4 +55,40 @@ int main()
     // ** COUNT HITS AND MISSES AND EVICTIONS!
     printSummary(hits, misses, evictions);
     return 0;
+}
+
+SetupInfo extractArgs(int argc, char *argv[]) {
+    int opt;
+    SetupInfo si = {0, 0, 0, NULL};
+    // put ':' in the starting of the
+    // string so that program can 
+    //distinguish between '?' and ':' 
+    while((opt = getopt(argc, argv, ":if:lrx")) != -1) 
+    { 
+        switch(opt) 
+        { 
+            case 's':
+                si.numSetIndexBits = atoi(optarg);
+                si.numSets = pow(2, si.numSetIndexBits);
+                break;
+            case 'E':
+                si.numLines = atoi(optarg);
+                break;
+            case 'b':
+                si.numBlockBits = atoi(optarg);
+                si.numBlocks = pow(2, si.numBlockBits);
+                break;
+            case 't':
+                si.traceFilename = optarg;
+                break;
+            case ':': 
+                printf("option needs a value\n"); 
+                break; 
+            case '?': 
+                printf("unknown option: %c\n", optopt);
+                break; 
+        } // end switch case
+    }  // end loop
+
+    // TODO: test this and print out the shtuff.
 }
